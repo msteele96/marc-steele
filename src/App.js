2913f748e7c422f001aca0613919139b40757b0a
msteele96 <marcsteele2014@gmail.com>
2021-09-07 14:20:07 -0400

    Basic Contact Component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Contact from './components/contact';
 
 function App() {
   return (
+  <>
     <h1 class="App-header">React App Under Construction</h1>
+    <Contact />
+  </>
   );
 }
 
---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <>
+            <h2>Linkedin: <a target={"_blank"} rel={"noreferrer"} href={"https://www.linkedin.com/in/msteele96/"}>msteele96</a></h2>
+            <h2>Github: <a target={"_blank"} rel={"noreferrer"} href={"https://github.com/msteele96"}>msteele96</a></h2>
+            <h2>Email: marcsteele2014@gmail.com</h2>
+            </>
+        )
+    }
+}
