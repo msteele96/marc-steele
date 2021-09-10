bf2d534ca8a01c7fc183c22bb13816e0e363adea
msteele96 <marcsteele2014@gmail.com>
2021-09-09 21:19:31 -0400

    Install React Router, add projects component and container

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,7 @@ export default class Contact extends Component {
             <>
             <h2>Linkedin: <a target={"_blank"} rel={"noreferrer"} href={"https://www.linkedin.com/in/msteele96/"}>msteele96</a></h2>
             <h2>Github: <a target={"_blank"} rel={"noreferrer"} href={"https://github.com/msteele96"}>msteele96</a></h2>
-            <h2>Email: marcsteele2014@gmail.com</h2>
+            <h2>Email: <a href={"mailto:marcsteele2014@gmail.com"}>marcsteele2014@gmail.com</a></h2>
             </>
         )
     }
