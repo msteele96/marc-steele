4bb89e84d2eb5223ae12268b93f025ff492af540
msteele96 <marcsteele2014@gmail.com>
2021-09-09 22:08:46 -0400

    Implemented basic routing to contact page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Contact from './components/contact';
+import { Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
   <>
     <h1 class="App-header">React App Under Construction</h1>
-    <Contact />
+
+    <Link to="/">Home</Link>
+    <Link to="/contact">Contact</Link>
+
+    <Switch>
+      <>
+      <Route path="/">
+        
+      </Route>
+
+      <Route exact path="/contact">
+        <Contact/>
+      </Route>
+
+      </>
+    </Switch>
+
   </>
   );
 }
