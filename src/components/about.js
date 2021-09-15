97e5a10de6f57b666db4d34fda931aad98bc0f33
msteele96 <marcsteele2014@gmail.com>
2021-09-15 16:35:42 -0400

    Added logos to about page. Need to format

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+
 export default class About extends Component {
     render() {
         return (
@@ -12,6 +13,12 @@ export default class About extends Component {
                     self-paced online software engneering program. Because of my personal interests in sports and the outdoors, and the 
                     recent focus on health and wellness, I would love to be able to help advance the technology in those fields.
                 </p>
+                <div class="logos">
+                    <img id="Flatiron" src={process.env.PUBLIC_URL + 'Flatiron_School_Logo.png'} alt="Flatiron School Logo" />
+                    <img id="Exeter" src={process.env.PUBLIC_URL + 'Phillips_Exeter_Academy_Seal.png'} alt="Phillips Exeter Academy Logo" />
+                    <img id="Springfield" src={process.env.PUBLIC_URL + 'Springfield College Logo.png'} alt="Springfield College Logo" />
+                </div>
+
             </>
         )
     }
