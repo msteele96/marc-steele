9856fa64d11c9abb8aac98d2c1aa2e9fd3782f5a
msteele96 <marcsteele2014@gmail.com>
2021-09-22 18:53:34 -0400

    Added link to resume on contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,11 +3,12 @@ import React, { Component } from "react";
 export default class Contact extends Component {
     render() {
         return (
-            <>
+        <>
             <h2>Linkedin: <a target={"_blank"} rel={"noreferrer"} href={"https://www.linkedin.com/in/msteele96/"}>msteele96</a></h2>
             <h2>Github: <a target={"_blank"} rel={"noreferrer"} href={"https://github.com/msteele96"}>msteele96</a></h2>
             <h2>Email: <a href={"mailto:marcsteele2014@gmail.com"}>marcsteele2014@gmail.com</a></h2>
-            </>
+            <a target={"_blank"} rel={"noreferrer"} href={process.env.PUBLIC_URL + "Marc_Steele_Resume_SE.pdf"}><h2>Resume</h2></a>
+        </>
         )
     }
 }
